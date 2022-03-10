c713a5a4ca8fedb7f76610d64bf3f5ea20645dbd
david.sf <david.sf@AN0101715066240.dinfor.uniceub>
2022-03-10 20:35:11 -0300

    Testando Commit na Sala 1017

--- TTMKT_Software/src/negocio/Funcionario.java
+++ TTMKT_Software/src/negocio/Funcionario.java
@@ -2,6 +2,6 @@ package negocio;
 
 public class Funcionario {
 	public static void main(String[] args) {
-		System.out.println("Testando Commit no Computador do David");
+		System.out.println("Testando Commit na Sala 1017");
 	}
 }
