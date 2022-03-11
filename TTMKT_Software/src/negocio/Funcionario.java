ccca81ad515edc96ab5f7d0e16d5cfc094c4511d
joao.msantana <joao.msantana@AN0101736077348.dinfor.uniceub>
2022-03-10 21:27:39 -0300

    Testando Commit no PC do João

--- TTMKT_Software/src/negocio/Funcionario.java
+++ TTMKT_Software/src/negocio/Funcionario.java
@@ -2,6 +2,6 @@ package negocio;
 
 public class Funcionario {
 	public static void main(String[] args) {
-		System.out.println("Testando Commit na Sala 1017");
+		System.out.println("Testando Commit no PC do João");
 	}
 }
