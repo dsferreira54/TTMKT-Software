682b13dc34cd4f7f9f86126f628b10f7815f903f
david.sf <david.sf@AN0101614NS4S9Y.dinfor.uniceub>
2022-04-04 21:16:23 -0300

    Criação do ProdutoMarcaContratoDAO

--- TTMKT_Software/src/persistencia/ProdutoMarcaContratoDAO.java
+++ TTMKT_Software/src/persistencia/ProdutoMarcaContratoDAO.java
@@ -1,5 +1,60 @@
 package persistencia;
 
-public class ProdutoMarcaContratoDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import negocio.Marca;
+
+public class ProdutoMarcaContratoDAO {
+	//PROPRIEDADES DA CLASSE
+	private BancoDeDados objBanco = null;
+	private PreparedStatement objExecucao = null;
+	private ResultSet objCursor = null;
+	
+	//MÉTODO GET TODOS
+	public Collection<Marca> getTodos() throws Exception {
+		ArrayList<Marca> retorno = new ArrayList<Marca>();
+		
+		objBanco = new BancoDeDados();
+		
+		objBanco.conectar();
+		
+		objExecucao = objBanco.getObjConexao().prepareStatement("SELECT * FROM PRODUTOMA_");
+		
+		objCursor = objExecucao.executeQuery();
+		
+		while (objCursor.next()) {
+			retorno.add(new Marca(objCursor.getString("CNPJ_MARCA"),
+								  objCursor.getString("TELEFONE"),
+								  objCursor.getString("EMAIL"),
+								  objCursor.getString("NOME")));
+		}
+		
+		objBanco.desconectar();
+		
+		return retorno;
+	}
+	
+	//MÉTODO INCLUIR
+	public void persistir(Marca objMarca) throws Exception {
+		objBanco = new BancoDeDados();
+		
+		objBanco.conectar();
+		
+		objExecucao = objBanco.getObjConexao().prepareStatement("INSERT INTO MARCA " +
+																"(CNPJ_MARCA, TELEFONE, EMAIL, NOME) " + 
+																"VALUES" +
+																"(?, ?, ?, ?)");
+		
+		objExecucao.setString(1, objMarca.getCnpjDaMarca());
+		objExecucao.setString(2, objMarca.getTelefone());
+		objExecucao.setString(3, objMarca.getEmail());
+		objExecucao.setString(4, objMarca.getNome());
+		
+		objExecucao.executeUpdate();
+		
+		objBanco.desconectar();
+	}
 }
